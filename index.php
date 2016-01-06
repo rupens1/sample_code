d080d39dba8c6ed9c59d76c21bf12cc9a2c56cce
rupens1 <rupenksheth@gmail.com>
2016-01-05 16:16:43 -0800

    Create index.php
    This is an index file for the event production website which is under production created for testing purpose to expand the business.

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>
+      IEP
+    </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/theme.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link href="assets/bxslider/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.theme.css">
+
+    <link href="css/superfish.css" rel="stylesheet" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'> -->
+
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="css/component.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <link rel="stylesheet" type="text/css" href="css/parallax-slider/parallax-slider.css" />
+    <script type="text/javascript" src="js/parallax-slider/modernizr.custom.28468.js">
+    </script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js">
+    </script>
+    <script src="js/respond.min.js">
+    </script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <!--header start-->
+<?PHP
+INCLUDE("header.php");
+?>   
+
+   <!--header end-->
+
+    <!-- Sequence Modern Slider -->
+    <div id="da-slider" class="da-slider">
+
+            <div class="da-slide">
+            
+      </div>
+
+
+            <div class="da-slide">
+            <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+       
+        
+      
+        <div class="da-img">
+         
+        </div>
+      </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="da-slide">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+       
+        <div class="da-img">
+          
+        </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      <nav class="da-arrows">
+        <span class="da-arrows-prev">
+        </span>
+        <span class="da-arrows-next">
+        </span>
+      </nav>
+    </div>
+
+
+    
+    
+
+
+<div class="container">
+      <div class="row mar-b-50">
+        <div class="col-md-12">
+          <div class="text-center feature-head wow fadeInDown">
+            <h1 class="">
+              Event
+            </h1>
+          </div>
+          <div class="feature-box">
+            <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
+              <div class="feature-box-heading">
+                <em>
+               
+ <img src="img/n/boda.png" alt="" width="100" height="100">
+                </em>
+                <h4>
+                  <b>WEDDING</b>
+                </h4>
+              </div>
+              <p class="text-center">
+               We believe everyone deserves a ‘Happily Ever After’ and shouldn’t be afraid to dream of a truly exceptional wedding!
+              </p>
+            </div>
+            <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
+              <div class="feature-box-heading">
+                <em>
+                  <img src="img/2.jpg" alt="" width="100" height="100">
+                </em>
+                <h4>
+                  <b>INTIMATE GATHERING</b>
+                </h4>
+              </div>
+              <p class="text-center">
+                We make your moments of gathering of friends and family with endless happiness and joy.
+              </p>
+            </div>
+            <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
+              <div class="feature-box-heading">
+                <em>
+                  <img src="img/n/4.jpg" alt="" width="100" height="100" style="
+    border-radius: 40px;
+">
+                </em>
+                <h4>
+                  <b>SOCIAL EVENT</b>
+                </h4>
+              </div>
+              <p class="text-center">
+                Looking for something unexpected to do with your group? Let us help you plan a truly memorable event on.
+              </p>
+            </div>
+      </div>
+</div>
+        </div>
+    </div>
+    
+    <div class="container">
+      <div class="row mar-b-50">
+        <div class="col-md-12">
+          
+          <div class="feature-box">
+             <div class="col-md-6 col-sm-6 text-center wow fadeInUp">
+              <div class="feature-box-heading">
+                <em>
+                  <img src="img/n/5.png" alt="" width="100" height="100">
+                </em>
+                <h4>
+                  <b>POLITICAL & GOVERNMENT</b>
+                </h4>
+              </div>
+              <p class="text-center">
+                We provide services for political events, political rallies, Advertisements in electronic media and organizing political party meetings for various sections and groups.
+              </p>
+            </div>
+            
+             <div class="col-md-6 col-sm-6 text-center wow fadeInUp">
+              <div class="feature-box-heading">
+                <em>
+                  <img src="img/n/6.jpg" alt="" width="100" height="100" style="
+    border-radius: 40px;
+">
+                </em>
+                <h4>
+                  <b>CORPORATE & NON-PROFIT</b>
+                </h4>
+              </div>
+              <p class="text-center">
+                We’ve provide best services to our corporate and private clients, and we know how to create an unforgettable experience for your clients, partners, or teams. 
+              </p>
+            </div>
+      </div>
+</div>
+        </div>
+    </div>
+
+
+    <!--property start-->
+    <div class="property gray-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-sm-6 text-center wow fadeInLeft">
+            <img src="img/about us.png" alt="" style="width:400px; height:519px;">
+          </div>
+          <div class="col-lg-6 col-sm-6 wow fadeInRight">
+            <h1>
+              About Us
+            </h1>
+            <hr>
+            <p>
+              <i class="fa fa-check fa-lg pr-10">
+              </i>
+            Impressionistic event management have a team of seasoned professionals that are committed to making sure that your event is successful, professionally organized and managed. Impressionistic can create concepts that are uniquely sculpted to reflect your brand, your message and your objectives.  
+            </p>
+            <p>
+              <i class="fa fa-check fa-lg pr-10">
+              </i>
+             We can help you to achieve your goals by communicating with your target audience. Including our expertise, and the ability to save our clients both time and money, Impressionistic offers a large number of services to help with your event.
+            </p>
+            <p>
+              <i class="fa fa-check fa-lg pr-10">
+              </i>
+              We can manage you entire event or help with just part of it.At Impressionistic, we understand your brand’s needs and requirements. Our focus and prime objective is to help you steer your brand into an unmatchable level of positioning amongst the target audience. 
+            </p>
+            <hr>
+            <a href="about.php" class="btn btn-purchase">
+             Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--property end-->
+
+    
+
+    <!--footer start-->
+    <?php
+	include("footer.php");	
+	?>
+    <!--small footer end-->
+
+    <!-- js placed at the end of the document so the pages load faster
+<script src="js/jquery.js">
+</script>
+-->
+    <script src="js/jquery-1.8.3.min.js">
+    </script>
+    <script src="js/bootstrap.min.js">
+    </script>
+    <script type="text/javascript" src="js/hover-dropdown.js">
+    </script>
+    <script defer src="js/jquery.flexslider.js">
+    </script>
+    <script type="text/javascript" src="assets/bxslider/jquery.bxslider.js">
+    </script>
+
+    <script type="text/javascript" src="js/jquery.parallax-1.1.3.js">
+    </script>
+    <script src="js/wow.min.js">
+    </script>
+    <script src="assets/owlcarousel/owl.carousel.js">
+    </script>
+
+    <script src="js/jquery.easing.min.js">
+    </script>
+    <script src="js/link-hover.js">
+    </script>
+    <script src="js/superfish.js">
+    </script>
+    <script type="text/javascript" src="js/parallax-slider/jquery.cslider.js">
+    </script>
+    <script type="text/javascript">
+      $(function() {
+
+        $('#da-slider').cslider({
+          autoplay    : true,
+          bgincrement : 100
+        });
+
+      });
+    </script>
+
+
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js">
+    </script>
+
+    <script type="text/javascript">
+      jQuery(document).ready(function() {
+
+
+        $('.bxslider1').bxSlider({
+          minSlides: 5,
+          maxSlides: 6,
+          slideWidth: 360,
+          slideMargin: 2,
+          moveSlides: 1,
+          responsive: true,
+          nextSelector: '#slider-next',
+          prevSelector: '#slider-prev',
+          nextText: 'Onward →',
+          prevText: '← Go back'
+        });
+
+      });
+
+
+    </script>
+
+
+    <script>
+      $('a.info').tooltip();
+
+      $(window).load(function() {
+        $('.flexslider').flexslider({
+          animation: "slide",
+          start: function(slider) {
+            $('body').removeClass('loading');
+          }
+        });
+      });
+
+      $(document).ready(function() {
+
+        $("#owl-demo").owlCarousel({
+
+          items : 4
+
+        });
+
+      });
+
+      jQuery(document).ready(function(){
+        jQuery('ul.superfish').superfish();
+      });
+
+      new WOW().init();
+
+
+    </script>
+  </body>
+</html>
